a9d906ae70925c4fee85498a10601f21d0ef4b09
Lamchi01 <sonic24072003@yandex.ru>
2025-03-05 00:13:42 +0300

    Небольшие доработки

--- telemetry/collector/src/main/java/ru/yandex/practicum/model/hub/HubEvent.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/model/hub/HubEvent.java
@@ -23,13 +23,10 @@ import java.time.Instant;
         @JsonSubTypes.Type(value = ScenarioAddedEvent.class, name = "SCENARIO_ADDED"),
         @JsonSubTypes.Type(value = ScenarioRemovedEvent.class, name = "SCENARIO_REMOVED")
 })
-@Getter
-@Setter
-@ToString
+@Getter @Setter @ToString
 public abstract class HubEvent {
     @NotBlank
     private String hubId;
-    @NotNull
     private Instant timestamp = Instant.now();
 
     @NotNull
