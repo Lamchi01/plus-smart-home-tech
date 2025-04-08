c220b66159df2231bb40607a335f0377f327c1f9
Lamchi01 <sonic24072003@yandex.ru>
2025-04-08 19:52:33 +0300

    Поправил поиск по пакету Feign клиентов

--- commerce/shopping-cart/src/main/java/ru/yandex/practucum/ShoppingCart.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practucum/ShoppingCart.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import ru.yandex.practicum.WarehouseOperations;
 
 @SpringBootApplication
-@EnableFeignClients(basePackageClasses = WarehouseOperations.class)
+@EnableFeignClients(basePackages = "ru.yandex.practicum")
 @EnableDiscoveryClient
 public class ShoppingCart {
     public static void main(String[] args) {
